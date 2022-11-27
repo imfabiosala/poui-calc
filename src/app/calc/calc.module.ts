7681e2bdab215ce7bf91dfd8fb5fecdfc678da13
Fábio Sala <imfabiosala@gmail.com>
2022-11-27 10:52:28 -0300

    [FEAT] Set. "app-calc" selector.

--- src/app/calc/calc.module.ts
+++ src/app/calc/calc.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CalcComponent } from './components'; /* Import Calc Module (index) */
+
+/* import { CalcComponent } from './components/calc.component'; Import Calc Module */
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CalcComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    CalcComponent
   ]
 })
 
