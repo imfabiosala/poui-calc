3092b4c690beffff54fc657d6a8b4f96122e4678
Fábio Sala <imfabiosala@gmail.com>
2022-11-27 08:12:01 -0300

    [FEAT] Import calc module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { CalcModule } from './calc/calc.module'; /* Import Calc Module */
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalcModule /* Import Calc Module */
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
