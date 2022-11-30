0325a345e5bf96150d6d286249b866d585c68772
Fábio Sala <imfabiosala@gmail.com>
2022-11-30 10:28:37 -0300

    [FEAT] Add. calc. basic operations.

--- src/app/calc/services/calc.service.ts
+++ src/app/calc/services/calc.service.ts
@@ -5,5 +5,49 @@ import { Injectable } from '@angular/core';
 })
 export class CalcService {
 
+  /* Definição das constantes utilizadas para as operações de cálculo. */
+
+  /* static   = indentifica que esta variável estará disponível em "escopo global". */
+  /* readonly = identifica que esta variável será uma constante, ou seja, o seu valor não sofrerá alteração durante toda a execução. */
+  /* SUM      = identifica o nome da variável. (não faz diferença se é maiúscula ou minúscula, porém, por padrão, quando se trata de constante é sempre utilizado maiúscula) */
+  /* string   = tipagem da variável */
+  /* '+'      = seu valor atribuído */
+
+  static readonly SUM: string = '+'; /* Soma */
+  static readonly SUB: string = '-'; /* Subtração */
+  static readonly DIV: string = '/'; /* Divisão */
+  static readonly MUL: string = '*'; /* Multiplicação */
+
+  calc(num1: number, num2: number, opr: string): number {
+
+    let res: number;
+
+    switch(opr) {
+    
+      case CalcService.SUM:
+        res = num1 + num2;
+        break;
+        
+      case CalcService.SUB:
+        res = num1 - num2;
+        break;
+
+      case CalcService.DIV:
+        res = num1 / num2;
+        break;
+
+      case CalcService.MUL:
+        res = num1 * num2;
+        break;
+
+      default:
+        res = 0;
+    
+    }
+
+    return res
+
+  }
+
   constructor() { }
 }
